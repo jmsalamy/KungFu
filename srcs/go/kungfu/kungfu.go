1654b934c19f5b0e09a7295b0b7849d5a81397ea
Ayush Sharma <ayushs@dhcp-18-21-148-248.dyn.mit.edu>
2020-03-05 14:27:52 -0500

    add new delayConfigs for experiments

--- srcs/go/kungfu/kungfu.go
+++ srcs/go/kungfu/kungfu.go
@@ -292,7 +292,7 @@ func (kf *Kungfu) parseIterationDelay() (Delay, bool) {
 
 func parseDelayConfigFile() map[int]Delay {
 	// pwd, _ := os.Getwd()
-	data, err := ioutil.ReadFile("/home/ghobadi_mit_edu/src/KungFu/config.txt")
+	data, err := ioutil.ReadFile("/home/ghobadi_mit_edu/src/KungFu/generated_configs/fifty.config")
 	if err != nil {
 		log.Errorf("File reading error", err)
 		return nil
